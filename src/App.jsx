8a39303f69bba607ced0599ab7d1f44d0ed04759
karlrouhana <karlrouhana58@gmail.com>
2024-06-13 16:38:37 +0300

    Add: Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Pages/Navbar/Navbar';
+import Home from './Pages/Home/Home';
 
 const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Home />
+    </>
   )
 }
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../../Components/Button/Button';
+import Pic1 from '../../Assets/LandingPageAssets/confused.svg';
+import Pic2 from '../../Assets/LandingPageAssets/browsingUnis.svg';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='home__content'>
+                <h1>Elevate Your Learning Experience Aboard</h1>
+                <p>Education Basket is a platform empowering students to seamlessly apply to multiple universities worldwide through a single application, backed by expert counselors every step of the way.</p>
+                <div className='home__content-buttons'>
+                    <Button value='Get Started' color='yellow' large={true} />
+                    <Button value='How it Works?' color='green' large={true} />
+                </div>
+            </div>
+            <div className="home__lines">
+                <div className="line-1">
+                    <img src={Pic1} alt="Image for Line 1" className="line-image" />
+                </div>
+                <div className="line-2"></div>
+                <div className="line-3">
+                    <img src={Pic2} alt="Image for Line 3" className="line-image" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
