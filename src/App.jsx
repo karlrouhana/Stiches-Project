eed03ddf7f08eb922990019629dd77d806dce22d
karlrouhana <karlrouhana58@gmail.com>
2024-06-13 20:53:42 +0300

    Add: Stage Component (Not Finished)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Navbar from './Pages/Navbar/Navbar';
 import Home from './Pages/Home/Home';
+import Stage from './Components/Stage/Stage';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Stage  title='Steamlined Application Submition' 
+              content='Apply to multiple institutions seamlessly through one platform, simplifying the process.' 
+              color='pink' 
+              faded={true}
+      />
     </>
   )
 }
--- src/Components/Stage/Stage.jsx
+++ src/Components/Stage/Stage.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import dots from '../../Assets/LandingPageAssets/dots.svg';
 import './stage.css';
 
-const Stage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+const Stage = (props) => {
+    const getClassForColor = () => {
+        switch (props.color) {
+            case 'yellow':
+                return 'stage-yellow';
+            case 'pink':
+                return 'stage-pink';
+            case 'green':
+                return 'stage-green';
+            default:
+                return '';
+        }
+    };
+
+    return (
+        <div className={`stage ${props.faded ? 'faded' : ''}`}>
+            <div className={`stage__header ${getClassForColor()}`}>
+                <p>{props.title}</p>
+                <img src={dots} alt="dots" />
+            </div>
+
+            <div className='stage__content'>
+                <p>{props.content}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Stage
