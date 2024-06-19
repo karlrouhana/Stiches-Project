ec8cbd7d08bed9f4012c0756460ce2590fe3cbe6
karlrouhana <karlrouhana58@gmail.com>
2024-06-19 10:49:10 +0300

    Add: Button Motion

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './button.css';
 
 const Button = (props) => {
@@ -15,10 +16,26 @@ const Button = (props) => {
         }
     };
 
+    const buttonVariants = {
+        initial: { opacity: 0, y: -10 },
+        animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+        hover: {
+            translateY: 5,
+            boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)",
+            transition: { duration: 0.3 },
+        }
+    };
+
     return (
-        <button className={`button ${getClassForColor()} ${props.large ? 'btn-large' : ''}`}>
+        <motion.button
+            className={`button ${getClassForColor()} ${props.large ? 'btn-large' : ''}`}
+            variants={buttonVariants}
+            initial="initial"
+            animate="animate"
+            whileHover="hover"
+        >
             {props.value}
-        </button>
+        </motion.button>
     );
 };
 
